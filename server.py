6afaefe1a273c52c8b0df115764040f0abc42b7d
tp1050 <tp1050@gmail.com>
2023-01-28 14:48:27 +0330

    first commit

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app=Flask(__name__,instance_relative_config=True)
+
+app.route('/')
+def index():
+    return 'Dool'
+
+
+@app.route('login',method=['POST', 'GET'])
+def login():
+    pass
+
+@app.route('
+
+
+app.run(host="0.0.0.0",port=7018,debug=True)
+
